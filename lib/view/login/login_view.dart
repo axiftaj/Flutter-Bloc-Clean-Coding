172b20c6769a925317e53ec3a4e87ea37fc544c4
Asif Taj <axiftaj@gmail.com>
2024-02-28 13:05:45 +0500

    login screen code updated

--- lib/view/login/login_view.dart
+++ lib/view/login/login_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+import '../../bloc/login_bloc/login_bloc.dart';
+import 'widget/widgets.dart';
 
 
 
@@ -13,33 +16,62 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
 
 
-  final emailFocusNode = FocusNode();
-  final passwordFocusNode = FocusNode();
+  final _emailFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    emailFocusNode.dispose();
-    passwordFocusNode.dispose();
+  void initState() {
+    super.initState();
+    _emailFocusNode.addListener(() {
+      if (!_emailFocusNode.hasFocus) {
+        context.read<LoginBloc>().add(EmailUnfocused());
+        FocusScope.of(context).requestFocus(_passwordFocusNode);
+      }
+    });
+    _passwordFocusNode.addListener(() {
+      if (!_passwordFocusNode.hasFocus) {
+        context.read<LoginBloc>().add(PasswordUnfocused());
+      }
+    });
   }
 
   @override
-  Widget build(BuildContext context) {
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
 
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        centerTitle: true,
-      ),
-      body: SafeArea(
+      body: BlocListener<LoginBloc , LoginStates>(
+        listener: (context, state){
+          if (state.status.isSuccess) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            showDialog<void>(
+              context: context,
+              builder: (_) => const SuccessDialog(),
+            );
+          }
+          if (state.status.isInProgress) {
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(
+                const SnackBar(content: Text('Submitting...')),
+              );
+          }
+        },
         child: Padding(
-          padding: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(8),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-            
+            children: <Widget>[
+              EmailInput(focusNode: _emailFocusNode),
+              PasswordInput(focusNode: _passwordFocusNode),
+              const SubmitButton(),
             ],
           ),
         ),
--- lib/view/login/widget/email_input_widget.dart
+++ lib/view/login/widget/email_input_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../../bloc/login_bloc/login_bloc.dart';
 
 
