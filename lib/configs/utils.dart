35587ac1dcd89917ab2ddcd83e14b1181bace2a3
Asif Taj <axiftaj@gmail.com>
2024-03-07 17:08:41 +0500

    optimising code

--- lib/configs/utils.dart
+++ lib/configs/utils.dart
@@ -6,12 +6,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 class Utils {
 
 
-  // we will use this function to shift focus from one text field to another text field
-  // we are using to avoid duplications of code
-  static void fieldFocusChange(BuildContext context , FocusNode current , FocusNode nextFocus){
-    current.unfocus();
-    FocusScope.of(context).requestFocus(nextFocus);
-  }
 
   // generic toast message imported from toast package
   // we will utilise this for showing errors or success messages
